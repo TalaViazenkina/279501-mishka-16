4aa10624d7f741a7725157bbf82c9e56fdac55ca
Natallia Viazenkina <natala.viaz@gmail.com>
2019-05-07 16:46:35 +0300

    Добавлены gulp.task("copy") и gulp.task("clean")

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,23 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var del = require("del");
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**",
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
